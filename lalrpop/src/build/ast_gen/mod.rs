11d98eef34f2412ca740462452737bd825db9b4f
Scanavium21 <scanavium21@protonmail.com>
2026-05-28 11:28:55 +0200

    Iterate nonterminals and collect some information

--- lalrpop/src/build/ast_gen/mod.rs
+++ lalrpop/src/build/ast_gen/mod.rs
@@ -2,5 +2,22 @@ use crate::grammar::parse_tree as pt;
 use crate::grammar::repr as r;
 
 pub fn emit_ast(grammar: &pt::Grammar, normalized_grammar: &r::Grammar) -> std::io::Result<Vec<u8>> {
+    for item in &grammar.items {
+        let nonterminal = match item {
+            pt::GrammarItem::Nonterminal(item) => item,
+            _ => continue,
+        };
+
+        let generate_name = match &nonterminal.type_decl {
+            Some(pt::TypeRef::Generate(name)) => name.clone(),
+            _ => continue,
+        };
+
+        let normalized_nonterminal = match normalized_grammar.nonterminals.get(&nonterminal.name) {
+            Some(data) => data,
+            None => continue,
+        };
+    }
+
     Ok(Vec::new())
 }
