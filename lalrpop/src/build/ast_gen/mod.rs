7d37adcaaa947fa824d2d276093eaa11f0e474b2
Scanavium21 <scanavium21@protonmail.com>
2026-05-28 13:42:32 +0200

    Get argument types from actions

--- lalrpop/src/build/ast_gen/mod.rs
+++ lalrpop/src/build/ast_gen/mod.rs
@@ -1,5 +1,7 @@
+use crate::collections::Map;
 use crate::grammar::parse_tree as pt;
 use crate::grammar::repr as r;
+use crate::grammar::repr::{ActionFnDefn, ActionFnDefnKind, TypeRepr};
 
 pub fn emit_ast(grammar: &pt::Grammar, normalized_grammar: &r::Grammar) -> std::io::Result<Vec<u8>> {
     let mut output = Vec::new();
@@ -19,7 +21,23 @@ pub fn emit_ast(grammar: &pt::Grammar, normalized_grammar: &r::Grammar) -> std::
             Some(data) => data,
             None => continue,
         };
+
+        for production in &normalized_nonterminal.productions {
+            let action = &normalized_grammar.action_fn_defns[production.action.index()];
+            let args = build_arg_map(action);
+        }
     }
 
     Ok(output)
+}
+
+fn build_arg_map(action: &ActionFnDefn) -> Map<String, TypeRepr> {
+    let mut map = Map::new();
+    if let ActionFnDefnKind::User(ref data) = action.kind {
+        for (pattern, ty) in data.arg_patterns.iter().zip(data.arg_types.iter()) {
+            let name = pattern.name();
+            map.insert(name, ty.clone());
+        }
+    }
+    map
 }
